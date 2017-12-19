36eb959295b01a2dd79fb5df759f697d3746b300
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2017-12-19 17:45:04 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# &lt;catalyst-flip-button&gt;
+
+`catalyst-flip-button` is a toggle button similar to [catalyst-toggle-button](https://gitlab.wgtn.cat-it.co.nz/rebeccastevens/catalyst-toggle-button).
+
+For compatibility reasons, this element extends HTMLElement instead of CatalystToggleButton.
+Because of this a lot of code in this repository is a duplicate of code from [catalyst-toggle-button](https://gitlab.wgtn.cat-it.co.nz/rebeccastevens/catalyst-toggle-button).
+
+## Usage
+
+Import the script for the web component on each page it is to be used on:
+
+```html
+<script src="dist/catalyst-flip-button.js"></script>
+```
+
+Then simply use it like any other tag:
+
+```html
+<catalyst-flip-button>My Button</catalyst-flip-button>
+```
+
+## Docs ans Demos
+
+Docs and demos are available on [gitlab pages](http://rebeccastevens.pages.gitlab.wgtn.cat-it.co.nz/catalyst-flip-button/).
+
+## Compatibility
+
+**Not all browser have full support for web components yet.**
+
+[WebComponentsJS](https://github.com/webcomponents/webcomponentsjs) is a set of polyfills that help fill that gap. [ShadyCSS](https://github.com/webcomponents/shadycss) is another polyfill that some browsers may need.
+
+Install via npm:
+
+```sh
+npm install --save @webcomponents/webcomponentsjs @webcomponents/shadycss
+```
+
+Then include on each page (before importing the component's efinition):
+
+```html
+<script src="node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
+<script src="node_modules/@webcomponents/shadycss/custom-style-interface.min.js"></script>
+```
+
+### ES5
+
+For older browser that don't support ES6 JavaScript, an ES5 transpiled versions is available (`*.es5.min.js`).
+
+To use this version, include it's script instead of the ES6 version and make sure each page that uses the component imports these scripts (before importing the component's definition):
+
+```html
+<script src="node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js"></script>
+<script src="node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
+<script src="node_modules/@webcomponents/shadycss/custom-style-interface.min.js"></script>
+```
+
+## Contributing
+
+### Dependencies
+
+Project dependencies are managed through [Yarn](https://yarnpkg.com/lang/en/docs/install/) (not npm).  
+Install dependencies with:
+
+```sh
+yarn
+```
+
+### Building
+
+[Gulp](https://gulpjs.com/) is used to build the source files (```./src```) into the distribution files (```./dist```).  
+Build with:
+
+```sh
+npm run build
+```
+
+### Docs
+
+Docs are build with [Polymer](https://www.polymer-project.org/), the [Polymer Build Tool](https://github.com/Polymer/polymer-build) and the [Polymer Analyzer](https://github.com/Polymer/polymer-analyzer).
+
+Docs will automatically be update on gitlab pages according to the ```analysis.json``` file whenever a change is made to the master branch.
+(Ideally the update should only happen when ```analysis.json``` is changed. [See here for details on this issue](https://gitlab.com/gitlab-org/gitlab-ce/issues/19232))
+
+To update ```analysis.json``` run:
+
+```sh
+npm run analyze
+```
+
+Please note that current the demo files are not automatically included in ```analysis.json``` and the different builds of the component will also be analyzed. Manually edit ```analysis.json``` to add the demos and remove the extra information on the different builds.  (This should be automatic in the future.)
+
+To build the docs manually:
+
+```sh
+npm run build-docs
+```
---
+++ dist/catalyst-flip-button.js
@@ -0,0 +1,385 @@
+/**
+ * A toggle button web component.
+ */
+(function() {
+
+    /**
+     * Create the custom element
+     */
+    function createElement() {
+
+      /**
+       * The name of the element tag.
+       */
+      const elementTagName = 'catalyst-flip-button';
+
+      /**
+       * Key codes.
+       */
+      const KEYCODE = {
+        SPACE: 32,
+        ENTER: 13
+      };
+
+      /**
+       * The template of the component.
+       */
+      const template = document.createElement('template');
+      template.innerHTML = `<style>:host{display:inline-block;align-items:flex-start;cursor:default;margin:0;padding:2px 7px;font:400 13.3333px Arial;text-rendering:auto;text-transform:none;text-indent:0;text-shadow:none;text-align:center;letter-spacing:normal;word-spacing:normal;-webkit-user-select:none;-moz-user-select:none;user-select:none;position:relative;width:auto;height:auto;color:#000000;overflow:hidden}:host #card{position:absolute;top:0;right:0;bottom:0;left:0;transform-style:preserve-3d;transition:transform .2s ease;perspective:800px;backface-visibility:hidden}:host #card figure{display:flex;align-items:center;justify-content:center;margin:0;position:absolute;top:0;right:0;bottom:0;left:0;backface-visibility:hidden;-webkit-appearance:var(--catalyst-flip-button-appearance, button);-moz-appearance:var(--catalyst-flip-button-appearance, button);appearance:var(--catalyst-flip-button-appearance, button)}:host #card figure.front{background:var(--catalyst-flip-button-front-background, #ddd)}:host #card figure.back{background:var(--catalyst-flip-button-back-background, #ddd);transform:rotateY(180deg)}:host([flipped]) #card{transform:rotateY(180deg)}:host(:focus){outline:none}:host(:focus) #card .front{outline:#000000 dotted 1px;outline:-webkit-focus-ring-color auto 5px}:host(:focus) #card .back{outline:none}:host([flipped]:focus) #card .front{outline:none}:host([flipped]:focus) #card .back{outline:#000000 dotted 1px;outline:-webkit-focus-ring-color auto 5px}:host([hidden]){display:none}
+</style><div id="card"><figure class="front">1</figure><figure class="back">2</figure></div>`;
+
+      // If using ShadyCSS.
+      if (window.ShadyCSS !== undefined) {
+        // Rename classes as needed to ensure style scoping.
+        ShadyCSS.prepareTemplate(template, elementTagName);
+      }
+
+      /**
+       * Class for the <catalyst-flip-button> element.
+       */
+      class CatalystFlipButton extends HTMLElement {
+
+        /**
+         * The attributes on this element to observe.
+         */
+        static get observedAttributes() {
+          return ['flipped', 'disabled', 'value', 'flipped-value'];
+        }
+
+        /**
+         * Construct the element.
+         */
+        constructor() {
+          super();
+
+          // Create a shadow root and stamp out the template's content inside.
+          this.attachShadow({mode: 'open'});
+          this.shadowRoot.appendChild(template.content.cloneNode(true));
+        }
+
+        /**
+         * Fires when the element is inserted into the DOM.
+         */
+        connectedCallback() {
+          // If using ShadyCSS.
+          if (window.ShadyCSS !== undefined) {
+            // Style the element.
+            ShadyCSS.styleElement(this);
+          }
+
+          // Upgrade the element's properties.
+          this._upgradeProperty('flipped');
+          this._upgradeProperty('disabled');
+          this._upgradeProperty('value');
+          this._upgradeProperty('flippedValue');
+
+          // No value set but textContent is set.
+          if (this.value === '' && this.textContent !== '') {
+            this.value = this.textContent;
+            this.textContent = null;
+          } else {
+            this._setUpDimensions();
+          }
+
+          // Set this element's role, tab index and aria attributes if they are not already set.
+          if (!this.hasAttribute('role')) {
+            this.setAttribute('role', 'button');
+          }
+          if (!this.hasAttribute('tabindex')) {
+            this.setAttribute('tabindex', 0);
+          }
+          if (!this.hasAttribute('aria-pressed')) {
+            this.setAttribute('aria-pressed', this.flipped);
+          }
+          if (!this.hasAttribute('aria-disabled')) {
+            this.setAttribute('aria-disabled', this.disabled);
+          }
+
+          // Add the element's event listeners.
+          this.addEventListener('keydown', this._onKeyDown);
+          this.addEventListener('click', this._onClick);
+        }
+
+        /**
+         * Upgrade the property on this element with the given name.
+         *
+         * A user may set a property on an _instance_ of an element before its prototype has been connected to this class.
+         * This method will check for any instance properties and run them through the proper class setters.
+         *
+         * See the [lazy properties](/web/fundamentals/architecture/building-components/best-practices#lazy-properties) section for more details.
+         *
+         * @param {string} prop
+         *   The name of a property.
+         */
+        _upgradeProperty(prop) {
+          // If the property exists.
+          if (this.hasOwnProperty(prop)) {
+            // Delete it and reset it.
+            let value = this[prop];
+            delete this[prop];
+            this[prop] = value;
+          }
+        }
+
+        _setUpDimensions() {
+          this.style.width = '';
+          this.style.height = '';
+
+          let style = getComputedStyle(this);
+          let width = Number.parseInt(style.width, 10);
+          let height = Number.parseInt(style.height, 10);
+
+          let adjustWidth = width === 0;
+          let adjustHeight = height === 0;
+
+          if (adjustWidth || adjustHeight) {
+            let card = this.shadowRoot.querySelector('#card');
+            let frontFace = card.querySelector('.front');
+            let backFace  = card.querySelector('.back');
+
+            card.style.position = 'relative';
+
+            frontFace.style.position = 'relative';
+            let w1 = adjustWidth  ? frontFace.offsetWidth  : 0;
+            let h1 = adjustHeight ? frontFace.offsetHeight : 0;
+            frontFace.style.position = '';
+
+            backFace.style.position = 'relative';
+            let w2 = adjustWidth  ? backFace.offsetWidth  : 0;
+            let h2 = adjustHeight ? backFace.offsetHeight : 0;
+            backFace.style.position = '';
+
+            card.style.position = '';
+
+            if (adjustWidth) {
+              this.style.width = (Math.max(w1, w2) + 2) + 'px';
+            }
+            if (adjustHeight) {
+              this.style.height = (Math.max(h1, h2) + 2) + 'px';
+            }
+          }
+        }
+
+        /**
+         * Fires when the element is removed from the DOM.
+         */
+        disconnectedCallback() {
+          this.removeEventListener('keydown', this._onKeyDown);
+          this.removeEventListener('click', this._onClick);
+        }
+
+        /**
+         * Setter for `flipped`.
+         *
+         * @param {*} value
+         *   If truthy, `flipped` will be set to true, otherwise `flipped` will be set to false.
+         */
+        set flipped(value) {
+          const isFlipped = Boolean(value);
+          if (isFlipped) {
+            this.setAttribute('flipped', '');
+          } else {
+            this.removeAttribute('flipped');
+          }
+        }
+
+        /**
+         * Getter for `flipped`.
+         */
+        get flipped() {
+          return this.hasAttribute('flipped');
+        }
+
+        /**
+         * Setter for `disabled`.
+         *
+         * @param {*} value
+         *   If truthy, `disabled` will be set to true, otherwise `disabled` will be set to false.
+         */
+        set disabled(value) {
+          const isDisabled = Boolean(value);
+          if (isDisabled) {
+            this.setAttribute('disabled', '');
+          }
+          else {
+            this.removeAttribute('disabled');
+          }
+        }
+
+        /**
+         * Getter for `disabled`.
+         */
+        get disabled() {
+          return this.hasAttribute('disabled');
+        }
+
+        /**
+         * Setter for `value`.
+         *
+         * @param {*} value
+         *   The value to set.
+         */
+        set value(value) {
+          this.setAttribute('value', new String(value));
+        }
+
+        /**
+         * Getter for `value`.
+         */
+        get value() {
+          if (!this.hasAttribute('value')) {
+            return '';
+          }
+
+          return this.getAttribute('value');
+        }
+
+        /**
+         * Setter for `flippedValue`.
+         *
+         * @param {*} value
+         *   The value to set.
+         */
+        set flippedValue(value) {
+          this.setAttribute('flipped-value', new String(value));
+        }
+
+        /**
+         * Getter for `flippedValue`.
+         */
+        get flippedValue() {
+          if (!this.hasAttribute('flipped-value')) {
+            return '';
+          }
+
+          return this.getAttribute('flipped-value');
+        }
+
+        /**
+         * Fired when any of the attributes in the `observedAttributes` array change.
+         *
+         * @param {string} name
+         *   The name of the attribute that changed.
+         * @param {*} oldValue
+         *   The original value of the attribute that changed.
+         * @param {*} newValue
+         *   The new value of the attribute that changed.
+         */
+        attributeChangedCallback(name, oldValue, newValue) {
+          const hasValue = newValue !== null;
+          switch (name) {
+            case 'flipped':
+              // Set the aria value.
+              this.setAttribute('aria-pressed', hasValue);
+              break;
+
+            case 'disabled':
+              // Set the aria value.
+              this.setAttribute('aria-disabled', hasValue);
+
+              // Add/Remove the tabindex attribute based `hasValue`.
+              if (hasValue) {
+                // If the tab index is set.
+                if (this.hasAttribute('tabindex')) {
+                  this._tabindexBeforeDisabled = this.getAttribute('tabindex');
+                  this.removeAttribute('tabindex');
+                  this.blur();
+                }
+              } else {
+                // If the tab index isn't already set and the previous value is known.
+                if (!this.hasAttribute('tabindex') && this._tabindexBeforeDisabled !== undefined && this._tabindexBeforeDisabled !== null) {
+                  this.setAttribute('tabindex', this._tabindexBeforeDisabled);
+                }
+              }
+              break;
+
+            case 'value':
+              let frontFace = this.shadowRoot.querySelector('.front');
+              frontFace.textContent = this.value;
+              this._setUpDimensions();
+              break;
+
+            case 'flipped-value':
+              let backFace = this.shadowRoot.querySelector('.back');
+              backFace.textContent = this.flippedValue;
+              this._setUpDimensions();
+              break;
+          }
+        }
+
+        /**
+         * Called when a key is pressed on this element.
+         *
+         * @param {KeyboardEvent} event
+         */
+        _onKeyDown(event) {
+          // Don’t handle modifier shortcuts typically used by assistive technology.
+          if (event.altKey) {
+            return;
+          }
+
+          // What key was pressed?
+          switch (event.keyCode) {
+            case KEYCODE.SPACE:
+            case KEYCODE.ENTER:
+              event.preventDefault();
+              this._togglePressed();
+              break;
+
+            // Any other key press is ignored and passed back to the browser.
+            default:
+              return;
+          }
+        }
+
+        /**
+         * Called when this element is clicked.
+         *
+         * @param {MouseEvent} event
+         */
+        _onClick(event) {
+          this._togglePressed();
+        }
+
+        /**
+         * `_togglePressed()` calls the `pressed` setter and flips its state.
+         * Because `_togglePressed()` is only caused by a user action, it will
+         * also dispatch a change event.
+         */
+        _togglePressed() {
+          // Don't do anything if disabled.
+          if (this.disabled) {
+            return;
+          }
+
+          // Change the value of flipped.
+          this.flipped = !this.flipped;
+
+          // This method is only caused by user action so dispatch a change event.
+          this.dispatchEvent(new CustomEvent('change', {
+            detail: {
+              flipped: this.flipped,
+            },
+            bubbles: true,
+          }));
+        }
+      }
+
+      // Make the class globally accessible.
+      window.CatalystFlipButton = CatalystFlipButton;
+
+      // Define the element.
+      window.customElements.define(elementTagName, CatalystFlipButton);
+    }
+
+    // If not using web component polyfills or if polyfills are ready, create the element.
+    if (window.WebComponents === undefined || window.WebComponents.ready) {
+      createElement();
+    }
+    // Otherwise wait until the polyfills is ready.
+    else {
+      window.addEventListener('WebComponentsReady', () => {
+        createElement();
+      });
+    }
+  })();
