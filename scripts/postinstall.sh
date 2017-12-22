834eb0c832a77e597bbb7f8f00cdca8a96e032da
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2017-12-22 18:49:31 +1300

    bower_components symlink is now created after install

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 gulp=node_modules/gulp/bin/gulp.js
 
-# Only fix dependency tree if not in node_modules.
+# Only run if not in node_modules.
 if [ \"${PWD##*/}\" != node_modules ]
 then
+  # Fix dependency tree
   node $gulp fix-dependency-tree
+
+  # Create bower_components symlink.
+  ln -sf ./node_modules/@bower_components ./bower_components
 fi
