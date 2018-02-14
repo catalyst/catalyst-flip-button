8fe5f382db00b5984a9d285f016a3ac0ab7c7b91
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-02-14 14:41:09 +1300

    Polymer elements are now loaded when ready as they auto register.

--- demo/import.js
+++ demo/import.js
@@ -1,14 +1,20 @@
-// Import the helpers.
-import '../node_modules/@polymer/iron-demo-helpers/demo-snippet.js';
-
 // Import the element.
 import { CatalystFlipButton } from '../node_modules/@catalyst-elements/catalyst-flip-button/dist/catalyst-flip-button.module.js';
+
+/**
+ * Load the polymer elements.
+ */
+function loadPolymerElements() {
+  import('../node_modules/@polymer/iron-demo-helpers/demo-snippet.js');
+}
 
 // Register the element.
 if (window.WebComponents === undefined || window.WebComponents.ready) {
   CatalystFlipButton.register();
+  loadPolymerElements();
 } else {
   window.addEventListener('WebComponentsReady', () => {
     CatalystFlipButton.register();
+    loadPolymerElements();
   });
 }
