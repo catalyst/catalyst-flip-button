67c1128486591bc248706d29e191f3f6e3437c0c
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2018-02-28 13:40:05 +1300

    Adjusted code style

--- src/element.js
+++ src/element.js
@@ -766,9 +766,9 @@ class CatalystFlipButton extends HTMLElement {
    * @see https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
    */
   _generateGuid() {
-    let s4 = () => {
+    const s4 = () => {
       return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    }
+    };
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
   }
 }
