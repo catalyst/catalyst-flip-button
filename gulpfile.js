4c6c54a727e2be9fd44df066ced2c072c0795924
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-03-26 20:25:16 +1300

    Switched to useing the catalyst-elements-build-process repo.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 /* eslint-env node */
 
-require('./tasks/clean.js');
+const gulp = require('gulp');
+const buildProcess = require('@catalyst-elements/build-process');
 
-require('./tasks/build.js'); // Task: build
-require('./tasks/analyze'); // Task: analyze
-require('./tasks/docs.js'); // Task: build-docs
-require('./tasks/lint.js'); // Task: lint
-require('./tasks/test.js'); // Task: test
-require('./tasks/fix-dependencies.js'); // Task: fix-dependencies
+buildProcess.setConfig('./package.json', {
+  componenet: {
+    name: 'catalyst-flip-button'
+  },
+
+  src: {
+    entrypoint: 'element.js',
+    template: {
+      html: 'template.html',
+      css: 'style.css'
+    }
+  }
+});
+
+for (const [name, func] of Object.entries(buildProcess.tasks)) {
+  gulp.task(name, func(gulp));
+}
